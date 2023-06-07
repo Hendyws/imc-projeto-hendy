74107c9ec90c415b617aaf0ff09054f175cc323a
hendersonabreu <henderson.abreu.silva@gmail.com>
2023-06-06 23:07:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculadora de IMC
+
+Projeto desenvolvido em React e Typescript.
+
+Desenvolvido a partir das aulas do curso B7web, incrementado com boas práticas.
---
+++ src/components/GridItens/gridItem.tsx
@@ -0,0 +1,29 @@
+import { Level } from "../../helpers/imc";
+import styles from './gridItem.module.css';
+import upImagem from '../../assets/up.png';
+import downImagem from '../../assets/down.png';
+
+
+type Props = {
+    item: Level
+}
+
+export const GridItem = ({item}: Props) => {
+    return(
+        <div className={styles.main} style={{backgroundColor: item.color}}>
+            <div className={styles.gridIcon}>
+                {/* {item.icon === 'up' ? <img src={upImagem} alt="" width={30}/> : ''}
+                {item.icon === 'down' ? <img src={downImagem} alt="" width={30}/> : ''} */}
+                <img src={item.icon === 'up' ? upImagem : downImagem} alt="" width={30}/>
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+            {item.yourImc ? 
+                <div className={styles.yourImc}>Seu IMC é de {item.yourImc} kg/m²</div>: ''}
+            <div className={styles.gridInfo}>
+                <>
+                    IMC entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+        </div>
+    )
+}
